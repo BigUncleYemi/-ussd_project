0978fa4d88c5feeba7a411803a2485b24d58e9ef
BigUncleYemi <olayemioyebanji911@gmail.com>
2018-03-10 13:33:32 +0100

    updated 404 page

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var bodyparser = require('body-parser');
 var app = express();
 var route = require('./route/route');
+var fs = require('fs');
 
 var port = process.env.PORT || (process.argv[2] || 3010);
 port = (typeof port === "number") ? port : 3010;
@@ -26,14 +27,21 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
 route(app);
-app.use(function(err, req, res, next){
-  console.log(`Error -> ${err}`);
-  res.send(`There is a problem somewhere but we are on it`);
-});
 
 app.use(function (req, res) {
   var four0four = fs.readFileSync(__dirname + '/404.html', 'utf-8')
   res.status(404).send(four0four)
+  next();
+});
+
+app.use(function(err, req, res, next){
+  if (res.err == [WritError]){
+    res.send(`This name has already being blacklisted do you want to update it`);
+  }
+})
+app.use(function(err, req, res, next){
+  console.log(`Error -> ${err}`);
+  res.send(`There is a problem somewhere but we are on it`);
 });
 
 require('./model/model');
