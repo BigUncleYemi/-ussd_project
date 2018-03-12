6eea57afb10337492b131c5c1544131c9474af22
BigUncleYemi <olayemioyebanji911@gmail.com>
2018-03-12 16:43:28 +0100

    upload of csv  and getting of csv data into database sucessful error handling in progress

--- route/route.js
+++ route/route.js
@@ -10,35 +10,67 @@ var Blacklist = mongoose.model('Blacklist');
 var Admin = mongoose.model('Admin');
 var fs = require('fs');
 var formidable = require('formidable');
+var csvjson = require('csvjson');
+var path = require('path')
 
 module.exports = function (app) {
-    app.post('/api/upload', function (req, res){
+    app.post('/api/upload', function (req, res) {
         var form = new formidable.IncomingForm();
-    
+
         form.parse(req);
-        form.maxFieldsSize = 2 * 1024 * 1024 * 1024;
+        form.maxFileSize = 2 * 1024 * 1024 * 1024;
         form.multiples = true;
-        form.on('fileBegin', function (name, file){
+        form.on('fileBegin', function (name, file) {
             file.path = __dirname + '/uploads/' + file.name;
         });
-        // form.on('progress', function (name, file){
-        //     res.writeHeader(200, ({
-        //         'Content-Type': 'text/html'
-        //     }));
-        //     var index = fs.readFileSync(__dirname + '/fileuploading.html', 'utf-8');
-        //     res.end(index);
-        // })
-        form.on('file', function (name, file){
+        form.on('file', function (name, file) {
             console.log('Uploaded ' + file.name);
+            var data = fs.readFileSync(path.join(__dirname + '/uploads/' + file.name ), {
+                encoding: 'utf8'
+            });
+            var options = {
+                delimiter: ',',
+                quote: '"'
+            };
+            var conv = csvjson.toSchemaObject(data, options);
+            console.log(conv)
+    
+            for (var i = 0; i < conv.length;) {
+                var new_blacklist = new Blacklist(conv[i]);
+                new_blacklist.save(function (err, results) {
+                    if (err) {
+                        console.log(`error - ${JSON.stringify(err)}`);
+    
+                    } else {
+                        Blacklist.count((err, count) => {
+                            console.log(`Count - ${count}`);
+                        });
+                        console.log(`results - ${JSON.stringify(results)}`);
+                        res.json({
+                            message: "Sucessful."
+                        });
+                        console.log(req.body)
+                        next(err)
+                    }
+                })
+                i++
+                // if(err){
+                //     res.json({
+                //         status:false,
+                //         error:'csv not sucessfully uploaded'
+                //     })
+                // }
+                // res.json({
+                //     message: 'uploaded csv into database'
+                // })
+            }
         });
-        form.on('error', function(err) {
+        form.on('error', function (err) {
             console.log('an error has occured while uploading')
         });
-        form.on('end', function(name, file) {
-            res.redirect('/api');
-        });
-    
+        console.log(form);
         res.sendFile(__dirname + '/index.html');
+
     });
 
     app.post('/api/sucessful', urlencodedparser, function (req, res, next) {
@@ -62,7 +94,7 @@ module.exports = function (app) {
         })
     });
 
-   
+
 
     app.get('/api', function (req, res) {
         res.writeHeader(200, ({
@@ -71,4 +103,28 @@ module.exports = function (app) {
         var index = fs.readFileSync(__dirname + '/index.html', 'utf-8');
         res.end(index);
     });
+
+    app.get('/api/search', function (req, res) {
+        res.writeHeader(200, ({
+            'Content-Type': 'text/html'
+        }));
+        var search = fs.readFileSync(__dirname + '/search.html', 'utf-8');
+        res.end(search);
+    });
+
+    app.get('/api/search/list', function (req, res) {
+        Blacklist.find({}, function (err, results) {
+            if (err) {
+                res.json({
+                    status: false,
+                    error: 'could not get list'
+                })
+            } else {
+                res.json({
+                    results,
+                    message: 'list found'
+                })
+            }
+        })
+    })
 }
--- test/test.js
+++ test/test.js
@@ -10,29 +10,32 @@ var expect = chai.expect;
 describe('# Api Test', function () {
 
     var blacklist = {
-        'MSISDN' : '2349058165478',
-        'operpator' : 'glo',
-        'categories' : 'Health'
+        'MSISDN': '2349058165478',
+        'operator': 'glo',
+        'categories': 'Health'
     }
+
+    // var test = require('./test.csv')
+
     describe('# Render form page', function () {
         after(function (done) {
             server.close();
             done();
         });
-        it('should render the form page', function(done) {
+        it('should render the form page', function (done) {
             request.get('/api').end(function (err, res) {
                 expect(res.statusCode).to.be.equal(200);
                 done();
             })
         })
     })
-    describe('# Post all maually inputed CSV ', function(){
+    describe('# Post all maually inputed CSV ', function () {
         after(function (done) {
             server.close();
             done();
         });
-        it('should post form details', function(done){
-            request.post('/api/sucessful').send(blacklist).end(function(err,res){
+        it('should post form details', function (done) {
+            request.post('/api/sucessful').send(blacklist).end(function (err, res) {
                 expect(res.statusCode).to.be.equal(200);
                 expect(res.body.message).to.be.equal('Sucessful.')
                 done();
@@ -40,13 +43,31 @@ describe('# Api Test', function () {
         })
     });
 
-    describe('# UPload all CSV file and read it', function(){
-        after(function(done){
+    // describe('# UPload all CSV file and read it', function(){
+    //     after(function(done){
+    //         server.close();
+    //         done();
+    //     });
+    //     it('should recieve csv file and upload its', function (done) {
+    //         request.post('/api/upload').send('test').end(function(err,res){
+    //             expect(res.statusCode).to.be.equal(200);
+    //             done();
+    //         })
+    //     })
+    // })
+
+    describe('# Get the full Blacklist', function () {
+        after(function (done) {
             server.close();
             done();
-        });
-        it('should recieve csv file and upload its', function (done) {
-            request.post('').send().e
+        })
+        it('should get full Blacklist files', function(done){
+            request.get('/api/search/list').end(function (err,res) {
+                expect(res.statusCode).to.be.equal(200);
+                expect(res.body.message).to.be.equal('list found');
+                console.log(res.body);
+                done();
+            })
         })
     })
 
