bfc9f707ee6a977ef82a810816f4317a885c9466
Oyebanji Olayemi Afolarin <olayemioyebanji911@gmail.com>
2018-04-21 17:15:52 +0100

    heroku test

--- model/model.js
+++ model/model.js
@@ -5,37 +5,28 @@ var Blacklist = new Schema({
     MSISDN: {
         type: Number,
         required: true,
-        unique: true
+        unique: true,
+        index:true
     },
-    Operator: {
+    operator: {
         type: String,
         required: true
     },
-    Categories: String,
+    categories: String,
     date_updated: {
         type: Date,
-        default: Date()
+        default: Date.now,
+        lowercase:true
     }
 });
 
-var Admin = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
+Blacklist.post('save', function(error, doc, next) {
+    if (error.name === 'MongoError' && error.code === 11000) {
+      next(new Error('There was a duplicate key error'));
+    } else {
+      next(error);
     }
-})
+  });
+  
 
 var Blacklist = mongoose.model('Blacklist', Blacklist);
-var Admin = mongoose.model('Admin', Admin);
-
-// // module.exports = {
-// //     Blacklist: Blacklist
-// // };
-// module.exports = {
-//     Admin: Admin
-// };
--- test/test.js
+++ test/test.js
@@ -10,35 +10,103 @@ var expect = chai.expect;
 describe('# Api Test', function () {
 
     var blacklist = {
-        MSISDN : '2349058165478',
-        Operator : 'glo',
-        Categories : 'Health'
+        'MSISDN': '2340058165478',
+        'operator': 'glo',
+        'categories': 'Health'
     }
-    describe('# Render form page', function () {
+    describe('# Post all maually inputed CSV ', function () {
         after(function (done) {
             server.close();
             done();
         });
-        it('should render the form page', function(done) {
-            request.get('/api').end(function (err, res) {
-                expect(res.statusCode).to.be.equal(200);
+        it('should post form details', function (done) {
+            request.post('/api/sucessful').send(blacklist)
+            .end(function (err, res) {
+                expect(res.statusCode).to.equal(200);
+                expect(res.body.message).to.be.equal('Sucessful.')
                 done();
             })
         })
-    })
-    describe('# Post CSV manually inputed', function () {
+    });
+    describe('# Get the full Blacklist', function () {
         after(function (done) {
             server.close();
             done();
-        });
-        it(' should get manually inputted details', function (done) {
-            request.post('/api/sucessful').send(blacklist).end(function (err, res) {
-                expect(res.statusCode).to.be.equal(200);
-                expect(res.body.blacklist.Categories).to.be.equal('Health');
-                blacklist = res.body;
+        })
+        it('should get full Blacklist files', function(done){
+            request.get('/api/search/list').end(function (err,res) {
+                expect(res.statusCode).to.equal(200);
+                expect(res.body.message).to.be.equal('list found');
+                console.log(res.body);
                 done();
             })
         })
     })
-
+    describe('# Get Operator info from Blacklist', function () {
+        after(function (done) {
+            server.close();
+            done();
+        })
+        it('should get a Blacklist Operator info', function(done){
+            request.get('/api/search/operator?operator=' + blacklist.operator).end(function (err,res) {
+                expect(res.statusCode).to.equal(200);
+                expect(res.body.message).to.be.equal('search completed');
+                console.log(res.body);
+                done();
+            })
+        })
+    })
+    describe('# Get MSISDN info from Blacklist', function () {
+        after(function (done) {
+            server.close();
+            done();
+        })
+        it('should get a Blacklist MSISDN info', function(done){
+            request.get('/api/search/MSISDN?MSISDN=' + blacklist.MSISDN).end(function (err,res) {
+                expect(res.statusCode).to.equal(200);
+                expect(res.body.message).to.be.equal('search completed');
+                console.log(res.body);
+                done();
+            })
+        })
+    })
+    describe('# Get Categories info from Blacklist', function () {
+        after(function (done) {
+            server.close();
+            done();
+        })
+        it('should get a Blacklist Categories info', function(done){
+            request.get('/api/search/categories?categories=' + blacklist.categories).end(function (err,res) {
+                expect(res.statusCode).to.equal(200);
+                expect(res.body.message).to.be.equal('search completed');
+                console.log(res.body);
+                done();
+            })
+        })
+    })
+    describe('Update a blacklist info', function() {
+        it('should modify a blacklist info', function(done) {
+          blacklist.categories = 'pop'
+          request
+            .put('/api/update/' + blacklist.MSISDN)
+            .send(blacklist)
+            .end(function(err, res) {
+              expect(res.body.message).to.equal('Update Sucessful');
+              expect(res.statusCode).to.equal(200);
+              console.log(res.body);
+              done();
+            });
+        });
+      });
+    describe('Delete all blacklist', function() {
+        it('should delete all blacklist', function(done) {
+          request
+            .delete('/api/delete/list')
+            .end(function(err, res) {
+              expect(res.statusCode).to.equal(200);
+              expect(res.body.message).to.equal('delete sucessful.');
+              done();
+            });
+        })
+    })
 })
